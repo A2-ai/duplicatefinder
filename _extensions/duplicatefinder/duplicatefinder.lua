18f970faf3ff1f5a651c20369df75604f3a85d28
Hyon-Oung Moon <ho@a2-ai.com>
2023-08-16 15:30:02 +0000

    updated version to stop printing every count

--- _extensions/duplicatefinder/duplicatefinder.lua
+++ _extensions/duplicatefinder/duplicatefinder.lua
@@ -2,14 +2,14 @@ local seen_identifiers = {}
 local identifier_counts = {}
 local seen_labels = {}
 local label_counts = {}
+local reported_duplicates = {} -- Keep track of reported duplicates
 
 function Pandoc(doc)
     for _, block in ipairs(doc.blocks) do
         local anchor = block.identifier
-        if anchor then
+        if anchor and anchor ~= "" then
             if seen_identifiers[anchor] then
                 identifier_counts[anchor] = (identifier_counts[anchor] or 1) + 1
-                quarto.log.error("Duplicate anchor identifier found: " .. anchor .. " (" .. identifier_counts[anchor] .. " times)")
             else
                 seen_identifiers[anchor] = true
             end
@@ -20,11 +20,24 @@ function Pandoc(doc)
             if label then
                 if seen_labels[label] then
                     label_counts[label] = (label_counts[label] or 1) + 1
-                    quarto.log.error("Duplicate LaTeX label found: " .. label .. " (" .. label_counts[label] .. " times)")
                 else
                     seen_labels[label] = true
                 end
             end
+        end
+    end
+
+    for anchor, count in pairs(identifier_counts) do
+        if count > 1 and not reported_duplicates[anchor] then
+            quarto.log.error("Duplicate anchor identifier found: " .. anchor .. " (" .. count .. " times)")
+            reported_duplicates[anchor] = true
+        end
+    end
+
+    for label, count in pairs(label_counts) do
+        if count > 1 and not reported_duplicates[label] then
+            quarto.log.error("Duplicate LaTeX label found: " .. label .. " (" .. count .. " times)")
+            reported_duplicates[label] = true
         end
     end
 
@@ -34,6 +47,3 @@ end
 return {
     {Pandoc = Pandoc}
 }
-
-
-
